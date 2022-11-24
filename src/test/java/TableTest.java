9d15f27ef09eac66eba3556ba03e742f654272aa
Svetlanushka1 <durovasvetlanushka@gmail.com>
2022-11-24 17:28:17 +0200

    HW_4
    // get row 4 content
    // check if there is text "Mexico" there
    // print amount of table rows
    // print amount of table cols
     // print content of row3
     // print content of last col
      // print number of row with name "Philip Cramer"

--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -7,29 +7,80 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.sql.SQLOutput;
+import java.util.List;
+
 public class TableTest {
 
     WebDriver wd;
 
 
     @BeforeMethod
-    public void init(){
+    public void init() {
         wd = new ChromeDriver();
         wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
     }
 
 
+    /* @Test
+     public void tableTestCSSCW(){
+         WebElement canada = wd.findElement(By.cssSelector("#customers tr:nth-child(8) td:last-child"));
+         String text = canada.getText();
+         System.out.println(text);
+         Assert.assertEquals(text,"Canada");
+     }
+     */
+
     @Test
-    public void tableTestCSSCW(){
-        WebElement canada = wd.findElement(By.cssSelector("#customers tr:nth-child(8) td:last-child"));
-        String text = canada.getText();
+    public void tableHW() {
+        // get row 4 content
+        WebElement company = wd.findElement(By.cssSelector("table#customers tr th:first-child"));
+        String text = company.getText();
+        Assert.assertEquals(text, "Company");
         System.out.println(text);
-        Assert.assertEquals(text,"Canada");
+
+        WebElement contact = wd.findElement(By.cssSelector("table#customers tr th:nth-child(2)"));
+        String text2 = contact.getText();
+        Assert.assertEquals(text2, "Contact");
+
+        WebElement helenBennett = wd.findElement(By.cssSelector("table#customers tr:nth-child(5) td:nth-child(2)"));
+        String text3 = helenBennett.getText();
+        //Assert.assertEquals(text3,"Helen Bennett");
+
+        // check if there is text "Mexico" there
+        //WebElement mexico = wd.findElement(By.cssSelector("table#customers tr:nth-child(4) td:last-child"));
+        String mexicoText = wd.findElement(By.cssSelector("table#customers tr:nth-child(4) td:last-child")).getText();
+        Assert.assertEquals(mexicoText, "Mexico");
+        Assert.assertTrue(mexicoText.contains("Mexico"));
+        wd.quit();
     }
-    
+
+    @Test
+    public void tableHW2() {
+        // print amount of table rows
+        List<WebElement> tableRows = wd.findElements(By.cssSelector("#customers tr"));
+        System.out.println(tableRows.size());
+
+        // print amount of table cols
+        List<WebElement> tableCols = wd.findElements(By.cssSelector("#customers tr th"));
+        System.out.println(tableCols.size());
+
+        // print content of row3
+        //System.out.println(tableCols.get(6).get(7).get(8));
+
+        // print content of last col
+        //List<WebElement> customersList = wd.findElements(By.cssSelector("#customers td"));
+
+
+        // print number of row with name "Philip Cramer"
+        String contact7 = wd.findElement(By.cssSelector("#customers tr:nth-child(7) td:nth-child(2)")).getText();
+        Assert.assertTrue(contact7.contains("Philip Cramer"));
+
+
+    }
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         wd.quit();
     }
 
